65a76c45269bf66c0ff89f2967f7b80038495a4e
git <466787459@qq.com>
2021-08-31 18:58:58 +0800

    Initial commit

---
+++ src/main/java/org/pengtao/flume/sink/SQLSink.java
@@ -0,0 +1,189 @@
+package org.pengtao.flume.sink;
+
+/**
+ * Created by yangyibo on 17/1/5.
+ */
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import org.apache.flume.*;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.sink.AbstractSink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class SQLSink extends AbstractSink implements Configurable {
+
+    private Logger LOG = LoggerFactory.getLogger(SQLSink.class);
+    private String hostname;
+    private String port;
+    private String databaseName;
+    private String tableName;
+    private String user;
+    private String password;
+    private PreparedStatement preparedStatement;
+    private Connection conn;
+    private int batchSize;
+//    private String url;
+    private String columns;
+    private String sql;
+    private String[] paras_list;
+    private Status result;
+    private Channel channel;
+    private Transaction transaction;
+    private Event event;
+    private String content;
+    List<String> contents = new ArrayList<>();
+    private String[] values;
+    private String value;
+
+    public SQLSink() {
+        LOG.info("SQLSink start...");
+    }
+
+    public void configure(Context context) {
+        hostname = context.getString("hostname");
+        Preconditions.checkNotNull(hostname, "hostname must be set!!");
+        port = context.getString("port");
+        Preconditions.checkNotNull(port, "port must be set!!");
+        databaseName = context.getString("databaseName");
+        Preconditions.checkNotNull(databaseName, "databaseName must be set!!");
+        tableName = context.getString("tableName");
+        Preconditions.checkNotNull(tableName, "tableName must be set!!");
+        user = context.getString("user");
+        Preconditions.checkNotNull(user, "user must be set!!");
+
+//        password = new String(Base64.getDecoder().decode(context.getString("password")));
+//        password = password.substring(0, password.indexOf(","));
+
+        password = context.getString("password");
+        Preconditions.checkNotNull(password, "password must be set!!");
+        batchSize = context.getInteger("batchSize", 100);
+        Preconditions.checkNotNull(batchSize > 0, "batchSize must be a positive number!!");
+//        url = context.getString("url");
+//        Preconditions.checkNotNull(url, "hostname must be url!!");
+        columns = context.getString("columns");
+        Preconditions.checkNotNull(columns, "columns must be set!!");
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        try {
+            //调用Class.forName()方法加载驱动程序
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String url = "jdbc:mysql://" + hostname + ":" + port + "/" + databaseName +
+                "?useUnicode=true&characterEncoding=UTF-8";
+        //调用DriverManager对象的getConnection()方法，获得一个Connection对象
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            conn.setAutoCommit(false);
+            //创建一个Statement对象
+//            preparedStatement = conn.prepareStatement("insert into " + tableName +
+//                    " (content,status,create_time) values  (?,0,now())");
+            paras_list = columns.split(",");
+            IntStream.range(0, paras_list.length).forEach(i -> paras_list[i] = "?");
+            String parameters = Arrays.toString(paras_list).replace("[" ,"").replace("]","");
+            sql = String.format("insert into %s(%s) values (%s)",tableName,columns,parameters);
+            LOG.info(sql);
+            preparedStatement = conn.prepareStatement(sql);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+//            System.exit(1);
+        }
+
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        if (preparedStatement != null) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Status process() throws EventDeliveryException {
+        result = Status.READY;
+        channel = getChannel();
+        transaction = channel.getTransaction();
+        transaction.begin();
+        try {
+            for (int i = 0; i < batchSize; i++) {
+                event = channel.take();
+                if (event != null) {//对事件进行处理
+                    //event 的 body 为   "exec tail$i , abel"
+                    content = new String(event.getBody());
+                    if (content.split(",").length != paras_list.length) {
+                        LOG.error(String.format("'%s' separator number has not enough,skip this one", content));
+                        continue;
+                    }
+                    contents.add(content);
+
+                } else {
+                    result = Status.BACKOFF;
+                    break;
+                }
+            }
+
+            if (contents.size() > 0) {
+                preparedStatement.clearBatch();
+                for (String temp : contents) {
+                    values = temp.split(",");
+                    for (int i = 1; i <= values.length; i++) {
+                        value = values[i - 1];
+                        if (value.isEmpty() || value.equals("")) {
+                            preparedStatement.setString(i, null);
+                        } else {
+                            preparedStatement.setString(i, value);
+                        }
+                    }
+                    preparedStatement.addBatch();
+                }
+                preparedStatement.executeBatch();
+                conn.commit();
+
+            }
+            transaction.commit();
+            contents.clear();
+        } catch (Exception e) {
+            try {
+                transaction.rollback();
+            } catch (Exception e2) {
+                LOG.error("Exception in rollback. Rollback might not have been" +
+                        "successful.", e2);
+            }
+            LOG.error("Failed to commit transaction." +
+                    "Transaction rolled back.", e);
+            Throwables.propagate(e);
+        } finally {
+            transaction.close();
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/org/pengtao/flume/source/HibernateHelper.java
@@ -0,0 +1,179 @@
+package org.pengtao.flume.source;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import org.hibernate.CacheMode;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.transform.Transformers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.flume.Context;
+
+/**
+ * Helper class to manage hibernate sessions and perform queries
+ * 
+ * @author <a href="mailto:mvalle@keedio.com">Marcelo Valle</a>
+ *
+ */
+public class HibernateHelper {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(HibernateHelper.class);
+
+	private static SessionFactory factory;
+	private Session session;
+	private ServiceRegistry serviceRegistry;
+	private Configuration config;
+	private SQLSourceHelper sqlSourceHelper;
+	private String maxSql;
+	private String maxtime;
+	private String ts;
+	private SimpleDateFormat dateFormat;
+	private Date date;
+
+	/**
+	 * Constructor to initialize hibernate configuration parameters
+	 * @param sqlSourceHelper Contains the configuration parameters from flume config file
+	 */
+	public HibernateHelper(SQLSourceHelper sqlSourceHelper) {
+
+		this.sqlSourceHelper = sqlSourceHelper;
+		Context context = sqlSourceHelper.getContext();
+
+		/* check for mandatory propertis */
+		sqlSourceHelper.checkMandatoryProperties();
+
+		Map<String,String> hibernateProperties = context.getSubProperties("hibernate.");
+		Iterator<Map.Entry<String,String>> it = hibernateProperties.entrySet().iterator();
+		
+		config = new Configuration();
+		dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Map.Entry<String, String> e;
+		
+		while (it.hasNext()){
+			e = it.next();
+			config.setProperty("hibernate." + e.getKey(), e.getValue());
+		}
+
+	}
+
+	/**
+	 * Connect to database using hibernate
+	 */
+	public void establishSession() {
+
+		LOG.info("Opening hibernate session");
+
+		serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(config.getProperties()).build();
+		factory = config.buildSessionFactory(serviceRegistry);
+		session = factory.openSession();
+		session.setCacheMode(CacheMode.IGNORE);
+		
+		session.setDefaultReadOnly(sqlSourceHelper.isReadOnlySession());
+	}
+
+	/**
+	 * Close database connection
+	 */
+	public void closeSession() {
+
+		LOG.info("Closing hibernate session");
+
+		session.close();
+		factory.close();
+	}
+
+	/**
+	 * Execute the selection query in the database
+	 * @return The query result. Each Object is a cell content. <p>
+	 * The cell contents use database types (date,int,string...), 
+	 * keep in mind in case of future conversions/castings.
+	 * @throws InterruptedException 
+	 */
+	@SuppressWarnings("unchecked")
+	public List<List<Object>> executeQuery() throws InterruptedException {
+		
+		List<List<Object>> rowsList = new ArrayList<List<Object>>() ;
+		Query query;
+		
+		if (!session.isConnected()){
+			resetConnection();
+		}
+
+		//判断是否是配置增量更新的时间字段，有的话就按照时间增量更新，否则走原来的逻辑
+		if (sqlSourceHelper.isIncrementalQuery()){
+			maxSql = sqlSourceHelper.maxQuery();
+//			LOG.info("sql:"+maxSql);
+			List<List<Object>> max = session.createSQLQuery(maxSql).setResultTransformer(Transformers.TO_LIST).list();
+			maxtime = max.get(0).get(0).toString().substring(0,19);
+			try {
+				date = dateFormat.parse(maxtime);
+				ts = String.valueOf(date.getTime() / 1000);
+				query = session.createSQLQuery(sqlSourceHelper.buildQuery(ts));
+				if (sqlSourceHelper.getMaxRows() != 0){
+					query = query.setMaxResults(sqlSourceHelper.getMaxRows());
+				}
+				rowsList = query.setFetchSize(sqlSourceHelper.getMaxRows()).setResultTransformer(Transformers.TO_LIST).list();
+//				LOG.info("Current time is "+sqlSourceHelper.getCurrentIndex()+",and lasttime is " + ts);
+//				LOG.info("Records count: "+rowsList.size());
+			}catch (Exception e){
+				LOG.error("Exception thrown, resetting connection.",e);
+				resetConnection();
+			}
+			if (!rowsList.isEmpty()){
+				sqlSourceHelper.setCurrentIndex(ts);
+			}
+		//原有的逻辑
+		} else {
+			if (sqlSourceHelper.isCustomQuerySet()) {
+					query = session.createSQLQuery(sqlSourceHelper.buildQuery());
+
+				if (sqlSourceHelper.getMaxRows() != 0){
+					query = query.setMaxResults(sqlSourceHelper.getMaxRows());
+				}
+			}
+			else
+			{
+//				LOG.info("query sql:" + sqlSourceHelper.getQuery());
+				query = session
+						.createSQLQuery(sqlSourceHelper.getQuery())
+						.setFirstResult(Integer.parseInt(sqlSourceHelper.getCurrentIndex()));
+
+				if (sqlSourceHelper.getMaxRows() != 0){
+					query = query.setMaxResults(sqlSourceHelper.getMaxRows());
+				}
+			}
+
+			try {
+				rowsList = query.setFetchSize(sqlSourceHelper.getMaxRows()).setResultTransformer(Transformers.TO_LIST).list();
+			}catch (Exception e){
+				LOG.error("Exception thrown, resetting connection.",e);
+				resetConnection();
+			}
+
+			if (!rowsList.isEmpty()){
+				sqlSourceHelper.setCurrentIndex(Integer.toString((Integer.parseInt(sqlSourceHelper.getCurrentIndex())
+						+ rowsList.size())));
+			}
+		}
+		
+		return rowsList;
+	}
+
+	private void resetConnection() throws InterruptedException{
+		if(session.isOpen()){
+			session.close();
+			factory.close();
+		} else {
+			establishSession();
+		}
+		
+	}
+}
